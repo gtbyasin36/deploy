9986bd0d5da8f8a4c53d585e57c769adadcee3aa
gtbyasin36 <yasin@alterra.id>
2020-04-13 21:14:32 -0400

    lanjutan

---
+++ lcMVC/LiveCodeMVC/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+# from django.conf import settings
+# from django.conf.urls import url
+# from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+]
+
+# if settings.DEBUG:
+# 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
